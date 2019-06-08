1a3f3e46e9fc12d8836ddf7874230af23ab55d4b
Hao Luo <howlowck@gmail.com>
2019-06-07 21:20:30 -0500

    updated elix and fixed template in sample element

--- src/GreetElement.js
+++ src/GreetElement.js
@@ -1,13 +1,9 @@
-import { ReactiveElement, symbols } from '../node_modules/elix/src/elix.js';
-
+import { ReactiveElement, symbols, template } from '../node_modules/elix/src/elix.js';
 
 export default class GreetElement extends ReactiveElement {
-
-  get [symbols.template]() {
-    return `Hello, <slot></slot>.`;
+  get [symbols.template] () {
+    return template.html`Hello, <slot></slot>.`;
   }
-
 }
-
 
 customElements.define('greet-element', GreetElement);
